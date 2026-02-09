4776b0b624913beceb88b468815750dce7c0f41a
Darcy Bartolome <bartolomedarcy@gmail.com>
2026-02-09 10:49:17 -0700

    square attempt 2

--- hello.py
+++ hello.py
@@ -13,19 +13,25 @@ def main():
         sleep(0.05)
         
         crawler.do_action('turn left',2,speed)
-        sleep(0.05)
-        crawler.do_action('forward',5,speed)
-        sleep(0.05)
-        
         crawler.do_action('turn left',2,speed)
         sleep(0.05)
         crawler.do_action('forward',5,speed)
         sleep(0.05)
         
         crawler.do_action('turn left',2,speed)
+        crawler.do_action('turn left',2,speed)
         sleep(0.05)
         crawler.do_action('forward',5,speed)
         sleep(0.05)
+        
+        crawler.do_action('turn left',2,speed)
+        crawler.do_action('turn left',2,speed)
+        sleep(0.05)
+        crawler.do_action('forward',5,speed)
+        sleep(0.05)
+        
+        crawler.do_action('turn left',2,speed)
+        crawler.do_action('turn left',2,speed)
         
         crawler.do_step('stand',speed)
         sleep(1)
