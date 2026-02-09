6d545914a0bc7f9a159258adfa7cdfa04d8b648f
Nebila Wako <nebilawako@gmail.com>
2026-02-09 10:52:01 -0700

    pi crawler pose

--- hello.py
+++ hello.py
@@ -3,6 +3,9 @@ from time import sleep
 
 crawler = Picrawler()
 
+new_step=[[45, 45, -75], [45, 0, -75], [45, 0, -30], [45, 45, -75]]
+stand_step = crawler.move_list['stand'][0]
+
 def main():
 
     speed = 80
@@ -33,8 +36,13 @@ def main():
         crawler.do_action('turn left',2,speed)
         crawler.do_action('turn left',2,speed)
         
-        crawler.do_step('stand',speed)
-        sleep(1)
+        print(f"stand step: {stand_step}")
+        crawler.do_step(stand_step, speed)
+        sleep(3)
+        
+        print(f"new step: {new_step}")
+        crawler.do_step(new_step,speed)
+        sleep(3)
 
 if __name__ == "__main__":
     main()
