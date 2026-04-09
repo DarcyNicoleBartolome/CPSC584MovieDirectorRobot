6461928190565787e5a2af845ba6ec4ff0c51a20
Darcy Bartolome <bartolomedarcy@gmail.com>
2026-04-09 13:10:56 -0600

    revert to stand pos

--- gui_server.py
+++ gui_server.py
@@ -400,8 +400,9 @@ def handle_client(client_socket, client_address):
    print(f"Connection from {client_address}")
    global speed 
    speed = 90 # Reset speed to 90
-   setup = [[Y_DEFAULT, X_DEFAULT, Z_DEFAULT],[Y_DEFAULT, X_DEFAULT, Z_DEFAULT],[Y_DEFAULT, X_DEFAULT, Z_DEFAULT],[Y_DEFAULT, X_DEFAULT, Z_DEFAULT]]
-   crawler.do_step(setup, speed)
+   # setup = [[Y_DEFAULT, X_DEFAULT, Z_DEFAULT],[Y_DEFAULT, X_DEFAULT, Z_DEFAULT],[Y_DEFAULT, X_DEFAULT, Z_DEFAULT],[Y_DEFAULT, X_DEFAULT, Z_DEFAULT]]
+   # crawler.do_step(setup, speed)
+   crawler.do_step("stand", speed)
    
    try:
       while True:
